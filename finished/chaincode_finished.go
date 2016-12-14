7dd2c50839daef0aca35bae1519f397d90e1f30b
danielsouza85 <danielsouza85@gmail.com>
2016-12-14 11:23:29 -0500

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -69,7 +69,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
-		var msg = "Testando servico do BLOCKCHAIN - DANIEL"		
+		var msg = "Testando servico do BLOCKCHAIN - DANIELSOUZA"		
 		return []byte(msg), nil 		
 		//return t.read(stub, args)
 	}
